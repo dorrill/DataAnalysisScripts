7d7d77b51ed2a1b3986c7818ac1d0d5f87c67d3c
Ryan Dorrill <dorrill1@gmail.com>
2023-11-06 15:42:34 -0800

    Create cleanDateframe.py
    Creating this file to host these useful functions, in case I need to clean up NA values in a df in the future

---
+++ cleanDateframe.py
@@ -0,0 +1,28 @@
+#Shrinks the memory usage of integer values in a dataframe, and cleans out NA values
+
+import pandas as pd
+
+# make function
+def shrink_ints(df):
+    mapping = {}
+    for col in df.dtypes[df.dtypes=='int64[pyarrow]'].index:
+        max_ = df[col].max()
+        min_ = df[col].min()
+        if min_ < 0:
+            continue
+        if max_ < 255:
+            mapping[col] = 'uint8[pyarrow]'
+        elif max_ < 65_535:
+            mapping[col] = 'uint16[pyarrow]'
+        elif max_ <  4294967295:
+            mapping[col] = 'uint32[pyarrow]'
+    return df.astype(mapping)
+
+
+def clean_df_no_na(df):
+    return (df
+     .assign(**df.select_dtypes('string').replace('', 'Missing').astype('category'),
+             **{'Garage Yr Blt': df['Garage Yr Blt'].clip(upper=df['Year Built'].max())})
+     .pipe(shrink_ints)
+     .pipe(lambda df_: df_.assign(**df_.select_dtypes('number').fillna(0)))
+    )    
